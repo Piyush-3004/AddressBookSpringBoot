6bbdf8dad32070c62cee1a80b4acc3de09045a95
Piyush <piyushp0541@gmail.com>
2022-08-10 10:05:37 -0700

    Project Structure

---
+++ src/main/java/com/blz/AddressBook/Controller/AddressBookController.java
@@ -0,0 +1,10 @@
+package com.blz.AddressBook.Controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/addressbook")
+public class AddressBookController {
+
+	
+}
